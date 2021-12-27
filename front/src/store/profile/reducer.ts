98c74a9ea87b0879de478941e6ce6cb4bed54d73
acaiibawl <hagiya0719@gmail.com>
2021-12-27 15:34:36 +0900

    住所入力用のreduxを追加

--- front/src/store/profile/reducer.ts
+++ front/src/store/profile/reducer.ts
@@ -7,14 +7,24 @@ const init: Profile = {
   description: "",
   birthday: "",
   gender: "",
+  address: {
+    postalcode: "",
+    prefecture: "",
+    city: "",
+    restAddress:""
+  }
 };
 
-const profileReducer = reducerWithInitialState(init).case(
-  profileActions.setProfile,
-  (state, payload) => ({
+const profileReducer = reducerWithInitialState(init)
+.case(profileActions.setProfile, (state, payload) => ({
     // 元の状態を表すstateを新しく渡された状態を表すpayloadで上書きした状態を返す
     ...state,
     ...payload
+  }))
+  .case(profileActions.setAddress, (state, payload) => ({
+    // addressの内容だけ新しい状態で更新
+    ...state,
+    address: { ...state.address, ...payload}
   })
 );
 
